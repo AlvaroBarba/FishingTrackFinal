d140b6823a77fd3f371de526b585ab01c9f8b625
lenovo2 <lenovo2@PRACTICAS1PC>
2021-05-10 09:45:28 +0200

    Creando Temas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { TemasService } from './services/temas.service';
 
 @Component({
   selector: 'app-root',
@@ -13,15 +14,29 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private theme:TemasService
   ) {
     this.initializeApp();
   }
 
+   //ACTIVAR TEMAS
+
+   activateDarkMode(){
+    this.theme.enableDarkMode();
+  }
+
+  activateDefaultMode(){
+    this.theme.enableDefaultMode();
+  }
+
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
+      this.statusBar.styleBlackTranslucent();
       this.splashScreen.hide();
+      this.theme.modeSelected();
     });
   }
+
+
 }
---
+++ src/app/services/temas.service.ts
@@ -0,0 +1,55 @@
+import { DOCUMENT } from "@angular/common";
+import { Inject, Injectable, Renderer2, RendererFactory2 } from "@angular/core";
+
+const M_SELECT='ModeSelect';
+
+@Injectable({
+    providedIn: 'root'
+  })
+  export class TemasService {
+  
+    renderer2: Renderer2;
+  
+    constructor(private rendererFactory: RendererFactory2, @Inject(DOCUMENT) private document: Document, private saveMode: Storage) {
+      this.renderer2 = this.rendererFactory.createRenderer(null, null);
+    }
+  
+    //GUARDAR EL TEMA SELECCIONADO AL CERRA LA APP
+  
+    modeSelected(){
+      this.saveMode.get(M_SELECT).then(value=>{
+        if(value){
+          this.renderer2.addClass(this.document.body, value);
+        }else{
+          //this.renderer2.removeClass(this.document.body,'lightModeTheme');
+          this.renderer2.removeClass(this.document.body,'darkModeTheme');
+        }
+      })
+    }
+  
+  
+    //ACTIVAR Y DESACTIVAR LOS TEMAS PARA SU CORRECTO USO
+  
+    //MODO OSCURO (DARK MODE)
+    enableDarkMode() {
+      //this.renderer2.removeClass(this.document.body, 'lightModeTheme');
+      this.renderer2.addClass(this.document.body, 'darkModeTheme');
+      this.saveMode.set(M_SELECT,'darkModeTheme');
+    }
+    
+    //MODO CLARO (LIGHT MODE)
+   // enableLightMode() {
+     // this.renderer2.removeClass(this.document.body, 'darkModeTheme');
+     // this.renderer2.removeClass(this.document.body, 'rainbowModeTheme');
+     // this.renderer2.addClass(this.document.body, 'lightModeTheme');
+     // this.saveMode.set(M_SELECT,'lightModeTheme');
+    //}
+  
+    //MODO POR DEFECTO (DEFAULT MODE)
+    enableDefaultMode() {
+      this.renderer2.removeClass(this.document.body, 'darkModeTheme');
+      //this.renderer2.removeClass(this.document.body, 'lightModeTheme');
+      //this.renderer2.removeClass(this.document.body, 'rainbowModeTheme');
+      this.saveMode.set(M_SELECT,null);
+    }
+     }
