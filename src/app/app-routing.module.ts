15e5741b844e5125288e7cade30901f7e6837f23
lenovo2 <lenovo2@PRACTICAS1PC>
2021-05-11 10:15:39 +0200

    Cambio diseño de mapa y página de inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,7 +27,11 @@ const routes: Routes = [
   {
     path: 'tab5',
     loadChildren: () => import('./tab5/tab5.module').then( m => m.Tab5PageModule)
+  },  {
+    path: 'info',
+    loadChildren: () => import('./pages/info/info.module').then( m => m.InfoPageModule)
   }
+
 ];
 @NgModule({
   imports: [
