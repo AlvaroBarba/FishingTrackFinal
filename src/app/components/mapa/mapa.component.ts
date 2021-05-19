249e36e4225fa2324e9d1e1562b1cf22196c1797
lenovo <lenove@practicas>
2021-05-19 15:15:10 +0200

    mapa perfecto ya

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core'
-import { GeoJSON,geoJSON, Map, tileLayer } from 'leaflet';
+import { GeoJSON,geoJSON, Map, Marker, marker, Point, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-mapa',
@@ -14,6 +14,7 @@ export class MapaComponent implements OnInit {
   title:string;
   @Input() route;
   id:string = "";
+  center: any;
   
 
   constructor() { }
@@ -24,31 +25,33 @@ export class MapaComponent implements OnInit {
 
   public createMap() {
     this.created = true;
-    console.log("ID MAPA -> " + this.route);
     this.miMapa = new Map(this.route);
     tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(this.miMapa);
 
-  this.miMapa.setView([40,-3],5)
+  this.miMapa.setView([40,-3], 10);
 
     setTimeout(() => {
       this.miMapa.invalidateSize();
     }, 400);
-    console.log(this.line)
     let myroute:GeoJSON=new GeoJSON(JSON.parse(this.line));
     
     myroute.addTo(this.miMapa);
-    this.miMapa.fitBounds(myroute.getBounds())
+    this.miMapa.fitBounds(myroute.getBounds());
 
     this.miMapa.on("load",()=>{
-      let myroute:GeoJSON=new GeoJSON();
+    let myroute:GeoJSON=new GeoJSON();
     myroute.addTo(this.miMapa);
     myroute.addData({
       type:"FeatureCollection",
       features: [this.line]
     }as any);
     });
-  
+    this.miMapa.setZoom(17);
+    myroute.setStyle({
+      color: "red",
+      opacity: 60
+    });
   }
 }
